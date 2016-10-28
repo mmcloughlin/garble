5d177547a1ffdda4dd36475d620f289e52ea8afb
Michael McLoughlin <mmcloughlin@gmail.com>
2016-10-27 19:57:42 -0700

    initial package setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import re
+
+pkg_init_file = open('garble/__init__.py').read()
+metadata = dict(re.findall("__([a-z]+)__\s*=\s*'([^']+)'", pkg_init_file))
+
+def readme():
+    return open('./README.rst').read()
+
+setup(name='garble',
+      version=metadata['version'],
+      description='Randomize your data',
+      long_description=readme(),
+      url='https://github.com/mmcloughlin/garble',
+      author='Michael McLoughlin',
+      author_email='mmcloughlin@gmail.com',
+      license='MIT',
+      )
