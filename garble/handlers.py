28209c712ef263e9a063c8e952d2f3ea5b955894
Michael McLoughlin <mmcloughlin@gmail.com>
2016-10-27 21:42:17 -0700

    uuid4 handling

--- garble/handlers.py
+++ garble/handlers.py
@@ -1,5 +1,7 @@
 import string
 import random
+import re
+import uuid
 
 
 def type_predicate(type_):
@@ -7,10 +9,12 @@ def type_predicate(type_):
 
 
 def handle_dict(x):
+    print 'handle_dict'
     return {k: garble(v) for k, v in x.items()}
 
 
 def handle_list(x):
+    print 'handle_list'
     return [garble(v) for v in x]
 
 
@@ -20,19 +24,36 @@ def random_string(n, alphabet=string.letters):
 
 
 def handle_str(x):
+    print 'handle_str:', x
     return random_string(len(x))
 
 
 def handle_int(x):
+    print 'handle_int:', x
     digits = len(str(x))
     return random.randrange(10**(digits-1), 10**digits)
+
+
+UUID4_REGEX = re.compile('^[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}$')
+
+def is_uuid4(x):
+    if type(x) not in [str, unicode]:
+        return False
+    return bool(UUID4_REGEX.match(x))
+
+
+def handle_uuid4(x):
+    print 'handle_uuid:', x
+    return str(uuid.uuid4())
 
 
 HANDLERS = [
         (type_predicate(dict), handle_dict),
         (type_predicate(list), handle_list),
+        (is_uuid4, handle_uuid4),
         (type_predicate(str), handle_str),
         (type_predicate(unicode), handle_str),
+        (type_predicate(long), handle_int),
         (type_predicate(int), handle_int),
         ]
 
@@ -41,4 +62,4 @@ def garble(x):
     for predicate, handler in HANDLERS:
         if predicate(x):
             return handler(x)
-    return x
+    return None
--- test/test.py
+++ test/test.py
@@ -1,6 +1,7 @@
 import random
 
 import garble
+import garble.handlers
 
 
 def test_str():
@@ -15,5 +16,11 @@ def test_int():
     for digits in range(1, 20):
         x = 10 * x + random.randrange(10)
         res = garble.garble(x)
-        assert type(res) == int
+        assert type(res) in [int, long]
         assert len(str(res)) == digits
+
+
+def test_uuid4():
+    x = '09dac315-8e9e-45ef-ab2c-c7309d0be083'
+    res = garble.garble(x)
+    assert garble.handlers.is_uuid4(res)
