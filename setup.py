d7eb0b2503f70a13847f934d53334213ce3431a5
Michael McLoughlin <mmcloughlin@gmail.com>
2016-10-29 16:51:39 -0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import re
 
 pkg_init_file = open('garble/__init__.py').read()
 metadata = dict(re.findall("__([a-z]+)__\s*=\s*'([^']+)'", pkg_init_file))
 
-def readme():
-    return open('./README.rst').read()
-
 setup(name='garble',
       version=metadata['version'],
       description='Randomize your data',
-      long_description=readme(),
       url='https://github.com/mmcloughlin/garble',
       author='Michael McLoughlin',
       author_email='mmcloughlin@gmail.com',
       license='MIT',
+      packages=find_packages(),
       entry_points={
           'console_scripts': [
               'garble = garble.__main__:main',
